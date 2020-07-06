b27731c1e6d28756c71be1858aa045e8002eab2c
manzilz <55953935+manzilz@users.noreply.github.com>
2020-07-06 15:27:02 -0700

    Update matmul.py

--- blocksparse/matmul.py
+++ blocksparse/matmul.py
@@ -5,7 +5,7 @@ from __future__ import division
 from __future__ import print_function
 
 import numpy as np
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
 import scipy.sparse as sparse
 from tensorflow.python.framework import ops
 from tensorflow.python.ops.init_ops import Initializer
